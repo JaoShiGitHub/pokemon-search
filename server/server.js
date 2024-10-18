a484d64071f5ac606f18590486b941cf4fcb9aa7
Oshin Lilligirl <oshingp@Oshins-MacBook-Pro.local>
2024-10-18 20:59:55 +0700

    installed axios

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ const PORT = 2024;
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Hey Baby!");
+  res.send("Hey Baby!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`);
-})
+  console.log(`Server started on port: ${PORT}`);
+});
