f0d2f5059f4c8579ffa824694a5213586df8a966
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-10-18 19:11:44 +0700

    Test server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 2024;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hey Baby!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+})
