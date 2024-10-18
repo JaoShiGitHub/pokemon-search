5dfab8e613cc93659d891473f68cdc93f9f4e8ac
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-10-19 01:24:45 +0700

    Fetch data to our server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 const cors = require("cors");
 const PORT = 2024;
@@ -7,6 +8,19 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hey Baby!");
+});
+
+app.get("/pokemon/:name", async (req, res) => {
+  const pokemonName = req.params.name.toLowerCase();
+  try {
+    const response = await axios.get(
+      `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonName}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching user:", error.message);
+    res.status(404).json({ error: "Not found pokemon" });
+  }
 });
 
 app.listen(PORT, () => {
