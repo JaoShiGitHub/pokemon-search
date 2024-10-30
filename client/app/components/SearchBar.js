068d33fe89c877edf95170c51e7c91f51022da8d
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-10-30 13:29:43 +0700

    Fetch pokemon

--- client/app/components/SearchBar.js
+++ client/app/components/SearchBar.js
@@ -2,12 +2,24 @@
 
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 
 function SearchBar() {
+  const router = useRouter();
   const [inputSearch, setInputSearch] = useState("");
-  const [pokemon, setPokemon] = useState([])
+  const [pokemon, setPokemon] = useState([]);
 
   const handleInputSearch = (e) => setInputSearch(e.target.value);
+
+  const handleOnClickSearchBtn = async () => {
+    const response = await axios(
+      `http://localhost:2024/pokemon/${inputSearch}`
+    );
+    setPokemon(data);
+    const data = response.data;
+    router.push(`/pokemon/${response.results.name}`);
+    console.log(pokemon);
+  };
 
   return (
     <div>
@@ -17,7 +29,7 @@ function SearchBar() {
         placeholder="Search Pokemon"
         onChange={handleInputSearch}
       />
-      <button>search</button>
+      <button onClick={handleOnClickSearchBtn}>search</button>
     </div>
   );
 }
