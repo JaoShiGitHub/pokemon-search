2e87210ae61bf02950c495fc3c17fa91f9510533
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-11-02 23:55:35 +0700

    Update SearchBar.js

--- client/app/components/SearchBar.js
+++ client/app/components/SearchBar.js
@@ -1,24 +1,17 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 function SearchBar() {
   const router = useRouter();
   const [inputSearch, setInputSearch] = useState("");
-  const [pokemon, setPokemon] = useState([]);
 
   const handleInputSearch = (e) => setInputSearch(e.target.value);
 
   const handleOnClickSearchBtn = async () => {
-    const response = await axios(
-      `http://localhost:2024/pokemon/${inputSearch}`
-    );
-    setPokemon(data);
-    const data = response.data;
-    router.push(`/pokemon/${response.results.name}`);
-    console.log(pokemon);
+    const keyword = inputSearch;
+    router.push(`/results/${keyword}`);
   };
 
   return (
