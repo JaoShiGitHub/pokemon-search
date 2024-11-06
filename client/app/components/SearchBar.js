10c4e8460cf772b09ad1cddc916aff0e7e6ef7d8
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2024-11-07 01:18:34 +0700

    Border

--- client/app/components/SearchBar.js
+++ client/app/components/SearchBar.js
@@ -16,7 +16,7 @@ function SearchBar() {
       <section className="px-8 w-full flex flex-col items-center gap-7 rounded-md">
         <img alt="Pokémon" src="pokemon-group-pic.png" className="w-1/3" />
         <h1 className="text-white font-bold text-4xl">Pokémon Search</h1>
-        <div className="px-10 h-12 w-1/2 rounded-lg bg-[#1E1E1E] flex justify-center items-center">
+        <div className="px-10 h-12 w-1/2 rounded-lg bg-[#1E1E1E] hover:border-black hover:border-2 flex justify-center items-center">
           <input
             className="bg-[#1E1E1E] text-white outline-none w-full h-7 "
             type="text"
