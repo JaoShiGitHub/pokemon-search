eb8e71f52521aed6eae5db0a75c1680b21406a42
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2024-11-06 12:16:59 +0700

    Start search box

--- client/app/pokemon/page.js
+++ client/app/pokemon/page.js
@@ -31,26 +31,35 @@ function PokemonDetails() {
 
   const pokemonTypes = pokemonData.types;
   if (isLoading) return <p>Loading...</p>;
-  console.log(isLoading);
 
   console.log(pokemonData);
   console.log("Sprites :", pokemonData.sprites?.front_default);
 
-  console.log(isLoading);
-
   return (
-    <div className="bg-[#2C2C2C] h-screen flex flex-col justify-center items-center">
-      <section className="bg-[#1E1E1E] text-white flex w-4/6">
+    <div className="bg-[#2C2C2C] h-screen flex flex-col items-center ">
+      <div className="bg-[#1E1E1E] my-10 max-w-4xl w-full h-16 rounded-3xl flex justify-between px-10 py-5">
+        <input
+          className="w-full bg-[#1E1E1E] text-white mr-5 outline-none"
+          placeholder="Search for Pokémon Name or ID"
+        />
+        <button> 🔍</button>
+      </div>
+      <section className="bg-[#1E1E1E] py-12 rounded-[36px] text-white flex max-w-4xl w-full  justify-center items-center gap-14">
         <img
-          className="w-80"
+          className="w-[290px] "
           alt={pokemonData.name}
           src={pokemonData.sprites?.front_default}
         />
-        <div>
-          <h1 className="uppercase">
-            #{pokemonData.id} {pokemonData.name}
-          </h1>
-          <div>
+        <div className="mt-4">
+          <div className="flex flex-col gap-3">
+            <h1 className="uppercase text-lg mb-2">
+              #{pokemonData.id} {pokemonData.name}
+            </h1>
+            <div className="uppercase flex gap-x-10">
+              <span>glass</span>
+              <span>poison</span>
+            </div>
+            {/* <div>
             {pokemonTypes ? (
               pokemonTypes.map((type, index) => {
                 return <span key={index}>{type.name}</span>;
@@ -58,24 +67,34 @@ function PokemonDetails() {
             ) : (
               <h1>nooo</h1>
             )}
+          </div> */}
+            <div className="flex gap-4">
+              <span>Weight: {pokemonData.weight}</span>
+              <span>Height: {pokemonData.height}</span>
+            </div>
           </div>
-          <div>
-            <span>Weight: {pokemonData.weight}</span>
-            <span>Height: {pokemonData.height}</span>
-          </div>
-          <div>
-            {/*  <span>HP: {pokemonData.stats[0]?.base_stat}</span>
-         <span>Attack: {pokemonData.stats[1]?.base_stat}</span>
-          <span>Defense: {pokemonData.stats[2]?.base_stat}</span>
-          <span>Sp.Attack: {pokemonData.stats[3]?.base_stat}</span>
-          <span>Sp.Defense: {pokemonData.stats[4]?.base_stat}</span>
-          <span>Speed: {pokemonData.stats[5]?.base_stat}</span> */}
-            <span>HP: 45</span>
-            <span>Attack: 49</span>
-            <span>Defense: 49</span>
-            <span>Sp.Attack: 65</span>
-            <span>Sp.Defense: 65</span>
-            <span>Speed: 45</span>
+          <div className="mt-12 grid grid-cols-2 gap-x-12 gap-y-3">
+            <p className="flex justify-between font-bold">
+              HP <span className="font-normal">45</span>
+            </p>
+
+            <p className="flex justify-between">
+              Sp.Attack <span className="ml-12">65</span>
+            </p>
+
+            <p className="flex justify-between">
+              Attack <span>49</span>
+            </p>
+            <p className="flex justify-between">
+              Sp.Defense <span>65</span>
+            </p>
+
+            <p className="flex justify-between">
+              Defense <span>49</span>
+            </p>
+            <p className="flex justify-between">
+              Speed <span>65</span>
+            </p>
           </div>
         </div>
       </section>
@@ -86,3 +105,12 @@ function PokemonDetails() {
 }
 
 export default PokemonDetails;
+
+{
+  /*  <span>HP: {pokemonData.stats[0]?.base_stat}</span>
+         <span>Attack: {pokemonData.stats[1]?.base_stat}</span>
+          <span>Defense: {pokemonData.stats[2]?.base_stat}</span>
+          <span>Sp.Attack: {pokemonData.stats[3]?.base_stat}</span>
+          <span>Sp.Defense: {pokemonData.stats[4]?.base_stat}</span>
+          <span>Speed: {pokemonData.stats[5]?.base_stat}</span> */
+}
