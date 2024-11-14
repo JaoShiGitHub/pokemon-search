9c883c60d1ce533f2d8b852e8c6bed2581b0e30f
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2024-11-14 21:50:44 +0700

    Create NotFound.js

---
+++ client/app/components/NotFound.js
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <section className="bg-[#1E1E1E] shadow-xl mb-40 py-12 rounded-[36px] text-white flex max-w-4xl w-full  justify-center items-center">
+      Pokemon not found
+    </section>
+  );
+}
+
+export default NotFound;
