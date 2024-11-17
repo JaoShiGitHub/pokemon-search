c2cd938bda7c40ef9bb99f24972d1f34fd99ac2d
JaoShiGitHub <158509127+JaoShiGitHub@users.noreply.github.com>
2024-11-17 16:56:47 +0700

    Add a property

--- app/components/NotFound.js
+++ app/components/NotFound.js
@@ -1,7 +1,7 @@
-function NotFound() {
+function NotFound(message) {
   return (
     <section className="bg-[#1E1E1E] shadow-xl mb-40 py-12 rounded-[36px] text-white flex max-w-4xl w-full  justify-center items-center">
-      Pokemon not found
+      {message.resultMessage}
     </section>
   );
 }
